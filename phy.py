7de13f57b2e0a0f5f94465bd557d316f49070045
aryanshailech <114009109+aryanshailech@users.noreply.github.com>
2024-07-14 22:44:32 +0530

    added files

---
+++ phy.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, gunicorn
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
